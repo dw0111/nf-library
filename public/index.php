8b4f3eb80eae2057cb6bfdb5e286056919b203e7
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-03-24 13:58:35 +0100

    Add routing

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "Hello World!";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$number = 42 / 0;
+
+$uri = $_SERVER['REQUEST_URI'];
+$verb = $_SERVER['REQUEST_METHOD'];
+$routes = require __DIR__ . '/../config/routes.php';
+$response = '404 - Not found';
+
+foreach ($routes as $route) {
+  if ($verb === $route[0] && $uri === $route[1]) {
+    $response = call_user_func($route[2]);
+  }
+}
+
+
+echo $response;
